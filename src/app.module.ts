166f1b9f368acc6048f3e39f6571e40fdc918a21
Mateus Henrique <mateus.magalhaes@upflow.me>
2021-01-01 21:58:29 -0300

    adicao da conexao com o banco

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { JogadoresModule } from './jogadores/jogadores.module';
 import { PadraoModule } from './padrao/padrao.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
+const strConnection = 'mongodb+srv://mateushfm:oiapoque12@cluster0.ejos5.gcp.mongodb.net/smartRanking?retryWrites=true&w=majority'
+const mongoParams = { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false}
 @Module({
-  imports: [JogadoresModule, PadraoModule],
+  imports: [
+    MongooseModule.forRoot(strConnection,mongoParams),
+    JogadoresModule, 
+    PadraoModule
+  ],
   controllers: [],
   providers: [],
 })
