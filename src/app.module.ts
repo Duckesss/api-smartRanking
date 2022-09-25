c0334b09066b98c8038b697d560fd26a5e9a6097
Mateus Henrique <mateus.henrique223@gmail.com>
2022-09-25 03:53:01 -0300

    chore: remove console log

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,6 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PlayersModule } from './players/players.module';
 
-console.log(
-  process.env.MONGO_CONNECTION_URL,
-  '======== process.env.MONGO_CONNECTION_URL',
-);
-
 @Module({
   imports: [
     PlayersModule,
